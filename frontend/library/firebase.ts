4047fba579071feb0492c7bf7c456a5099c9a23e
Karen Chen <karen.chenlai@mail.mcgill.ca>
2025-03-09 21:11:36 -0400

    update firebase

--- frontend/library/firebase.ts
+++ frontend/library/firebase.ts
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtuXqByr5fUJSB5az_fZfmSUfvopxRJGQ",
-  authDomain: "lazycare8866.firebaseapp.com",
-  projectId: "lazycare8866",
-  storageBucket: "lazycare8866.appspot.com",
-  messagingSenderId: "1076620138730",
-  appId: "1:1076620138730:web:55b30c064c980af4211f93",
-  measurementId: "G-RF6CB6Y630"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-
